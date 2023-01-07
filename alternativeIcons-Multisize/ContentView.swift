d70e92ea3925ad350dfe9ec0000727cc9b9995a9
Peter Alt <peter@peteralt.com>
2023-01-07 09:28:32 -0700

    Initial Commit

---
+++ alternativeIcons-Multisize/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  alternativeIcons-Multisize
+//
+//  Created by Peter Alt on 1/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ alternativeIcons-Multisize/alternativeIcons_MultisizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  alternativeIcons_MultisizeApp.swift
+//  alternativeIcons-Multisize
+//
+//  Created by Peter Alt on 1/7/23.
+//
+
+import SwiftUI
+
+@main
+struct alternativeIcons_MultisizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
